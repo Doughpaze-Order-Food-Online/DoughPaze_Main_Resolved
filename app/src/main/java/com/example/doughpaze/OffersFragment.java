9b2f0ae593910a26c47c045024a667c43061e8cf
siddhantdrk <siddhantdrk@gmail.com>
2020-08-17 13:41:03 +0530

    offers fragment tab and viewpager done

--- app/src/main/java/com/example/doughpaze/OffersFragment.java
+++ app/src/main/java/com/example/doughpaze/OffersFragment.java
@@ -2,11 +2,16 @@ package com.example.doughpaze;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class OffersFragment extends Fragment {
 
+    ViewPager2 viewPager2;
 
 
     public OffersFragment() {
@@ -25,6 +31,28 @@ public class OffersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_offers, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_offers, container, false);
+
+        viewPager2 = rootView.findViewById(R.id.offers_viewPager);
+
+        viewPager2.setAdapter(new OfferFragmentStateAdapter(this));
+
+        TabLayout offers_tabs = rootView.findViewById(R.id.offers_tabs);
+
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+                offers_tabs, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0) {
+                    tab.setText("Restaurant Offers");
+                } else {
+                    tab.setText("Payment offers/coupons");
+                }
+            }
+        }
+        );
+        tabLayoutMediator.attach();
+
+        return rootView;
     }
 }
